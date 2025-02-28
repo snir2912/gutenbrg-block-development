53268825d8b533eb5724cef20f5a733d48014f98
snir2912 <you@example.com>
2025-02-28 11:27:50 +0200

    abc

---
+++ wp-content/plugins/curvy/build/blocks/curvy/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c90085582b3a1316e69d');
---
+++ wp-content/plugins/curvy/build/blocks/curvy/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1ff51f91bcb7d3007781');
